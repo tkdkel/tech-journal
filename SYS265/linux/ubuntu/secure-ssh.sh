cec9c77fe00fc3697be4671009f26000caa5d4c3
tkdkel <kel.kruk@mymail.champlain.edu>
2022-02-23 21:12:39 +0000

    ssh updated

--- SYS265/linux/ubuntu/secure-ssh.sh
+++ SYS265/linux/ubuntu/secure-ssh.sh
@@ -2,5 +2,10 @@
 #author tkdkel
 #creates a new ssh user using $l parameter
 #adds a public key from the local repo or curled from the remote repo
-#removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+#removes roots ability to ssh i
+useradd -m -d /home/testuser -s /bin/bash testuser
+mkdir /home/testuser/.ssh
+cp  SYS265/linux/public-keys/id_rsa.pub /home/testuser/.ssh/authorized_keys
+chmod 700 /home/testuser/.ssh
+chmod 600 /home/testuser/.ssh/authorized_keys
+chown -R testuser:testuser /home/testuser/.ssh
